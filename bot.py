0cf935484063577144d1b6a3bb681b182dea7eb0
chick_0 <chick_0@ch1ck.xyz>
2022-01-15 22:16:39 +0900

    날짜 & 시간 출력 개선

--- bot.py
+++ bot.py
@@ -1,6 +1,3 @@
-from NicoBot.discordapi.const import LIB_NAME
-from NicoBot.discordapi.slash import SlashCommand
-from NicoBot.discordapi.slash import DiscordInteractionClient
 import sys
 import logging
 from os import environ
@@ -10,7 +7,9 @@ from datetime import timezone
 from datetime import timedelta
 
 from dotenv import load_dotenv
-
+from NicoBot.discordapi.const import LIB_NAME
+from NicoBot.discordapi.slash import SlashCommand
+from NicoBot.discordapi.slash import DiscordInteractionClient
 
 load_dotenv()
 
@@ -27,14 +26,35 @@ handler.setLevel("INFO")
 
 @SlashCommand.create("시간 정보 로딩", ())
 def t(ctx):
-    pst = datetime.now(timezone(timedelta(hours=-8))).strftime("%Y년 %m월 %d일 %H시 %M분")
-    kst = datetime.now(timezone(timedelta(hours=9))).strftime("%Y년 %m월 %d일 %H시 %M분")
+    date_format = "%Y년 %m월 %d일 [ # ]     [ @ ] %I시 %M분"
+    date_map = {
+        "Sun": "***일요일***",
+        "Mon": "월요일",
+        "Tue": "화요일",
+        "Wed": "수요일",
+        "Thu": "목요일",
+        "Fri": "금요일",
+        "Sat": "**토요일**",
+    }
+    time_map = {
+        "AM": "*오전*",
+        "PM": "오후",
+    }
+
+    pst = datetime.now(timezone(timedelta(hours=-8)))
+    kst = datetime.now(timezone(timedelta(hours=+9)))
+
+    pst = pst.strftime(date_format) \
+        .replace("#", date_map.get(pst.strftime("%a"))) \
+        .replace("@", time_map.get(pst.strftime("%p")))
+
+    kst = kst.strftime(date_format) \
+        .replace("#", date_map.get(kst.strftime("%a"))) \
+        .replace("@", time_map.get(kst.strftime("%p")))
 
     return "\n".join([
-        "```",
-        f"PST {pst}",
-        f"KST {kst}",
-        "```"
+        f"= **PST** =   {pst}",
+        f"~~=~~ **KST** ~~=~~   {kst}",
     ])
 
 
